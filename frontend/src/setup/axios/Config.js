3cd381f34143819d62d4664528d94a11350d790d
waly2020 <walyguema@gmail.com>
2024-05-03 22:28:15 +0100

    base url

--- frontend/src/setup/axios/Config.js
+++ frontend/src/setup/axios/Config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "https://basecamp-pw67.onrender.com/",
+    baseURL : "https://basecamp-pw67.onrender.com",
     // baseURL : "http://localhost:10000",
     headers : {
         Accept : "*"
