99714d261463e8c3507337160efc27f3fa3b41e2
waly2020 <walyguema@gmail.com>
2024-05-03 22:22:34 +0100

    modif axios instance

--- frontend/src/setup/axios/Config.js
+++ frontend/src/setup/axios/Config.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:10000",
+    baseURL : "https://basecamp-pw67.onrender.com/login",
+    // baseURL : "http://localhost:10000",
     headers : {
         Accept : "*"
     }
     
 });
+
 // axios.interceptors.response.use((config) =>{
 //     console.log(config);
 //     console.log("Requette intercepte");
